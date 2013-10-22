a9ccf9d036411e1eb2284b885f6935f4913154af
Ian Macalinao <ianmacalinao@gmail.com>
2013-10-22 13:44:11 -0500

    Initial commit.
    Signed-off-by: Ian Macalinao <ianmacalinao@gmail.com>

---
+++ README.md
@@ -0,0 +1,8 @@
+Itemconomy
+========
+
+An item based economy for Minecraft.
+
+## License
+This is proprietary New Liberty code. Do not distribute it.
+
---
+++ src/main/java/net/new_liberty/itemconomy/ItemConomy.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.new_liberty.itemconomy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author simplyianm
+ */
+public class ItemConomy extends JavaPlugin {
+}
