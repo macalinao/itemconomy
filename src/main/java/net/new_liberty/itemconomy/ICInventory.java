641ac551a318b205f8140617dd8bedd1c96d0261
Ian Macalinao <ianmacalinao@gmail.com>
2013-10-22 19:43:43 -0500

    Create API to deal with inventories
    Signed-off-by: Ian Macalinao <ianmacalinao@gmail.com>

---
+++ src/main/java/net/new_liberty/itemconomy/ICInventory.java
@@ -0,0 +1,118 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.new_liberty.itemconomy;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ *
+ * @author simplyianm
+ */
+public class ICInventory {
+
+    private final Inventory i;
+
+    public ICInventory(Player p) {
+        this.i = p.getInventory();
+    }
+
+    /**
+     * Gets this player's balance.
+     *
+     * @return
+     */
+    public int balance() {
+        int sum = 0;
+        for (ItemStack s : stacks()) {
+            sum += Itemconomy.i().getValue(s.getType());
+        }
+        return sum;
+    }
+
+    /**
+     * Gets all pertinent ItemStacks.
+     *
+     * @return
+     */
+    public List<ItemStack> stacks() {
+        List<ItemStack> r = new ArrayList<ItemStack>();
+        for (ItemStack s : i.getContents()) {
+            for (Entry<Material, Integer> e : Itemconomy.i().currencySet()) {
+                if (s.getType() == e.getKey()) {
+                    r.add(s);
+                }
+            }
+        }
+        return r;
+    }
+
+    /**
+     * Removes an amount of currency from this inventory.
+     *
+     * @param amt
+     */
+    public void remove(int amt) {
+        // Sort using a tree map
+        SortedMap<Integer, Material> s = new TreeMap<Integer, Material>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o1 - o2;
+            }
+
+        });
+
+        // Place elements to sort
+        for (Entry<Material, Integer> e : Itemconomy.i().currencySet()) {
+            s.put(e.getValue(), e.getKey());
+        }
+
+        // Iterate backwards -- get rid of the blocks first
+        for (Entry<Integer, Material> e : s.entrySet()) {
+            int removed = amt / e.getKey();
+            int surplus = removed;
+
+            ItemStack[] contents = i.getContents();
+            for (int a = 0; a < contents.length; a++) {
+                ItemStack t = contents[a];
+
+                if (surplus == 0) {
+                    break;
+                }
+
+                if (t.getType() == e.getValue()) {
+                    // Find amt to remove
+                    int rem = t.getAmount();
+                    if (rem > surplus) {
+                        rem = surplus;
+                    }
+
+                    // Remove the item
+                    if (rem == 0) {
+                        i.clear(a);
+                    } else {
+                        t.setAmount(t.getAmount() - rem);
+                    }
+
+                    surplus -= rem;
+                }
+            }
+
+            // Recalculate amt of items removed
+            removed -= surplus;
+
+            amt -= removed * e.getKey();
+        }
+    }
+
+}
--- src/main/java/net/new_liberty/itemconomy/Itemconomy.java
+++ src/main/java/net/new_liberty/itemconomy/Itemconomy.java
@@ -4,6 +4,10 @@
  */
 package net.new_liberty.itemconomy;
 
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -13,8 +17,50 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class Itemconomy extends JavaPlugin {
 
-    public Material getCurrency() {
-        return Material.EMERALD;
+    private static Itemconomy _instance;
+
+    private Map<Material, Integer> currency = new EnumMap<Material, Integer>(Material.class);
+
+    @Override
+    public void onEnable() {
+        _instance = this;
+
+        // Our currency - TODO config
+        currency.put(Material.EMERALD, 1);
+        currency.put(Material.EMERALD_BLOCK, 9);
+    }
+
+    @Override
+    public void onDisable() {
+        _instance = null;
+    }
+
+    /**
+     * Gets the Itemconomy instance.
+     *
+     * @return
+     */
+    public static Itemconomy i() {
+        return _instance;
+    }
+
+    /**
+     * Gets a set of all the currency.
+     *
+     * @return
+     */
+    public Set<Entry<Material, Integer>> currencySet() {
+        return currency.entrySet();
+    }
+
+    /**
+     * Gets the value of a certain material in this economy.
+     *
+     * @param m
+     * @return
+     */
+    public int getValue(Material m) {
+        return currency.get(m);
     }
 
 }
